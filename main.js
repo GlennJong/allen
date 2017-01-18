4e97b5962671f5ed5dac046ad6edcd48711df368
CHH <chh@chh.tw>
2017-01-18 16:27:21 +0800

    git init

---
+++ main.js
@@ -0,0 +1,81 @@
+(function () {
+
+    'use strict'
+
+
+    var Allen = {}
+
+    Allen.canvas = document.getElementById('canvas')
+    Allen.ctx    = Allen.canvas.getContext('2d')
+    Allen.input  = document.getElementById('input')
+    Allen.submit = document.getElementById('submit')
+
+    Allen.init = function () {
+        this.submit.addEventListener('click', this._onSubmit.bind(this))
+    }
+
+    Allen._draw = function (text) {
+        this._loadImages(['./images/bg.jpg', './images/main.png'])
+            .then(this._drawLayer1.bind(this))
+            .then(this._drawText.bind(this, text))
+            .then(this._drawLayer2.bind(this))
+    }
+
+
+    Allen._loadImages = function (imagesURL) {
+
+        let promises = imagesURL.map((imageURL) => {
+
+            return new Promise((resolve, reject) => {
+                let image = new Image()
+                image.onload = () => resolve(image)
+                image.src = imageURL
+            })
+
+        })
+
+        return Promise.all(promises)
+    }
+
+
+    Allen._drawLayer1 = function (images) {
+        [this.image1, this.image2] = images
+
+        this.canvas.width  = this.image1.width
+        this.canvas.height = this.image1.height
+        this.ctx.drawImage(this.image1, 0, 0, this.canvas.width, this.canvas.height)
+    }
+
+    Allen._drawText = function (text) {
+        this.ctx.fillStyle = "white"
+        this.ctx.font = '40pt Calibri';
+        this.ctx.fillText(text, 10, 50)
+    }
+
+
+    Allen._drawLayer2 = function () {
+        this.ctx.drawImage(this.image2, 0, 396)
+    }
+
+
+    Allen._onSubmit = function (event) {
+
+        event.preventDefault()
+
+        if (!this.input.value) {
+            alert('請輸入文字！！！')
+        }
+        else {
+            this._draw.call(this, this.input.value)
+        }
+
+    }
+
+    Allen.init()
+
+
+    // expose
+    window.Allen = Allen
+
+
+})()
